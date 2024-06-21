6d86e7ef11cb7d2d96c08bff59578eaa96569717
bhramprakash <your-email@example.com>
2024-06-21 22:02:50 +0530

    Updated website with new features

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,12 +3,12 @@ import React from 'react';
 import './Carousel.css';
 
 const images = [
+  process.env.PUBLIC_URL + '/11.jpg',
+  process.env.PUBLIC_URL + '/14.png',
+  process.env.PUBLIC_URL + '/12.jpeg',
   process.env.PUBLIC_URL + '/3d.jpg',
-  process.env.PUBLIC_URL + '/1.png',
-  process.env.PUBLIC_URL + '/2.jpg',
-  process.env.PUBLIC_URL + '/3d.jpg',
-  process.env.PUBLIC_URL + '/1.png',
-  process.env.PUBLIC_URL + '/3.jpg',
+  process.env.PUBLIC_URL + '/15.jpg',
+  process.env.PUBLIC_URL + '/13.jpg',
   process.env.PUBLIC_URL + '/1.png',
   process.env.PUBLIC_URL + '/2.jpg',
 ];
